6accab7e563f4b065e84ad642fc663d6cb5b5bde
kauancamilosenai <kauan_camilo@estudante.sesisenai.org.br>
2026-04-29 14:23:54 -0300

    atualizado 100%
    tá atualizado mano

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
-import react, { useState, useEffect } from 'react';
-import axios from 'axion';
+import React, { useState, useEffect } from 'react';
+import axios from  'axios';
 import './App.css';
-
-const [pokemons, setPokemons] = useState ({});
-conste [loading, setLoading] = useState (true)
-const [error, setError] = useState (false)  
-
-userEffect(() => {
-  const getData = async () => {
-    try {
-      const res = await
-    axios.get('https//pokeapi.co/api/v2/pokemon/ditto');
-        serPokemons(res.data);
-        console.log('success:', res.data);
-        setLoading(false);
-    }
-    catch (err)  {
-      console.error ("erro ao carregar API", err);
-      setLoading(false)
-    setError (true)
-    }
-  };
-  getData();
-}, {});
 
 function App() {
 
-  if (loading) return <div 
-  className="loader"> carregando pokedex...
-  </div>;
+  const [pokemons, setPokemons] = useState({}); 
+  const [loading, setLoading] = useState(true); 
+  const [error, setError] = useState(false); 
+useEffect(() => {
+  const getData = async () => {
+    try {const res = await
+      axios.get('https://pokeapi.co/api/v2/pokemon/bulbasaur');
+      setPokemons(res.data);
+      console.log('Success:', res.data);
+      setLoading(false);
+    }
+    catch (err) {
+      console.error("Error ao carregar API", err); 
+      setLoading(false)
+      setError(true)
+    }
+  };
+  getData();
+}, {})
 
-  if (error) return <div className="error">
+
+
+
+
+  if (loading) return <div className='loader'>
+    Carregando Vagabundex
+    </div>;
+ 
+ if (error) return <div className='error'>
   ocorreu um erro inesperado
   </div>
 
   return (
     <div className="App">
-    <h1>pokemon</h1>
+      <h1> POKEMON list! </h1>
     </div>
   );
 }
