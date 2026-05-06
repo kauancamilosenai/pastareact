81f2101880a67fba02f016fd195cb3ae93120f73
kauancamilosenai <kauan_camilo@estudante.sesisenai.org.br>
2026-05-06 17:12:36 -0300

    atualizado (06/05/2026)
    começamos a fazer as coisas tudo tuffoxxxx

---
+++ src/home.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import {Route, BrowserRouter, Routes} from "react-router-dom";
+import './home.css'
+
+function Treinador(){
+    return(
+        <div className="titulo"> 
+        <h1>Treinador</h1>
+            <div className="imagem">
+                <div className="info">
+                <img src="https://cdn.meutimao.com.br/_upload/forum/2024/02/19/cassio_calca_quadrada_qq.jpg" alt="cassio bobe esponja"></img>
+                <h2>Kauan</h2>
+                <h2>9999+ anos</h2>
+                <h2>Cassio</h2>
+                <h2>tenho 1 poquemom só</h2>
+                <h2>conquistas zeradas</h2>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Treinador;
---
+++ src/pokemon.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import axios from  'axios';
+import './pokemon.css';
+
+function Pokemon() {
+
+  const [pokemons, setPokemons] = useState({}); 
+  const [loading, setLoading] = useState(true); 
+  const [error, setError] = useState(false); 
+
+useEffect(() => {
+  const getData = async () => {
+    try {const res = await
+      axios.get('https://pokeapi.co/api/v2/pokemon/charmander');
+      setPokemons(res.data);
+      console.log('Success:', res.data);
+      setLoading(false);
+    }
+    catch (err) {
+      console.error("Error ao carregar API", err); 
+      setLoading(false)
+      setError(true)
+    }
+  };
+  getData();
+}, {}) 
+
+  if (loading) return <div className='loader'>
+    Carregando...
+    </div>;
+ 
+ if (error) return <div className='error'>
+  ocorreu um erro inesperado
+  </div>;
+
+  return(
+    <div className="pokemon">
+      <h1>POKEMON CARD</h1>
+      <div className="pokemon-container">
+        <div className="pokemon-card">
+          <h3>{pokemons.name}</h3>
+          <h4>{pokemons.types[0].type.name}</h4>
+          <img src={pokemons.sprites.front_shiny} alt={pokemons.name}/>
+          <h3>{pokemons.stats[0].stat.name+' '+pokemons.stats[0].base_stat+' '+pokemons.stats[1].stat.name+' '+pokemons.stats[0].base_stat}</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Pokemon;
